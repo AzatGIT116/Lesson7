29a936ad6551dcb5255513f29891630bcf9f4e95
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-20 00:36:57 +0300

    add Zadacha 49

---
+++ Zadacha49/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты. */
+
+int[,] array = new int[3, 4];
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 9);
+        }
+    }
+}
+
+void CheckArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+            array [i,j] = Convert.ToInt32(Math.Pow(array [i,j], 2)); // Возведение в квадрат
+        }
+    }
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],2} | ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+CheckArray(array);
+PrintArray(array);
