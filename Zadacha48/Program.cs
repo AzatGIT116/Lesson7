89b8bd65e47e0e97ee6a47b6f804bd5bdd015d00
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-20 00:36:38 +0300

    add Zadacha48

---
+++ Zadacha48/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран. */
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write(" | ");
+            Console.Write($"{mtrx[i, j],1} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] result = GetMatrix(3, 4, 1, 20);
+PrintMatrix(result);
