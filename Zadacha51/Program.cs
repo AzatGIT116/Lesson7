56fc635dc82920c175d4e784a81d036b2cf84dd0
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-20 00:37:15 +0300

    add Zadacha 51

---
+++ Zadacha51/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д. */
+
+int[,] array = new int[3, 4];
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 9);
+        }
+    }
+}
+
+int CheckArray(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += array[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],2} | ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int sumall = CheckArray(array);
+Console.WriteLine(sumall);
